9b33e3912141ad2f534db5cc51826fc7d6ab4af3
Enhua <ez867@nyu.edu>
2026-02-14 18:19:32 -0500

    Upload

--- fms_option.h
+++ fms_option.h
@@ -67,7 +67,8 @@ namespace fms::option {
 		template<class F = double, class S = double, class K = double>
 		auto call(F f, S s, K k, const model<F, S>& m)
 		{
-			return 0; 
+			auto p = put(f, s, k, m);
+			return p + f - k;
 		}
 
 		// In the Black-Scholes/Merton model
@@ -96,6 +97,22 @@ namespace fms::option {
 
 			// TODO: implement bsm::put and bsm::call.
 			// Hint: use bsm_to_black to get f and s and then call black::put and black::call.
+
+			template<class F = double, class S = double>
+			inline auto put(double r, double s0, double sigma, double k, double t,
+				const model<F, S>& m)
+			{
+				auto [f, s] = bsm_to_black<F, S>(r, s0, sigma, t);
+				return black::put(f, s, k, m);
+			}
+
+			template<class F = double, class S = double>
+			inline auto call(double r, double s0, double sigma, double k, double t,
+				const model<F, S>& m)
+			{
+				auto [f, s] = bsm_to_black<F, S>(r, s0, sigma, t);
+				return black::call(f, s, k, m);
+			}
 		}
 	}
 
--- xll_option.cpp
+++ xll_option.cpp
@@ -39,6 +39,35 @@ double WINAPI xll_option_cdf(double x, double s, HANDLEX m)
 }
 // TODO: implement OPTION.CGF
 
+AddIn xai_option_cgf(
+	Function(XLL_DOUBLE, L"xll_option_cgf", CATEGORY L".CGF")
+	.Arguments({
+		Arg(XLL_DOUBLE, L"s", L"is the volatility."),
+		Arg(XLL_HANDLEX, L"m", L"is the handle to a model."),
+		})
+		.Category(CATEGORY)
+	.FunctionHelp(L"Return cumulant generating function (CGF) value of an option model.")
+);
+double WINAPI xll_option_cgf(double s, HANDLEX m)
+{
+#pragma XLLEXPORT
+	double result = NaN<double>;
+
+	try {
+		handle<model<>> m_(m);
+		ensure(m_);
+		result = m_->cgf(s);
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+	catch (...) {
+		XLL_ERROR(__FUNCTION__ ": unknown exception");
+	}
+
+	return result;
+}
+
 AddIn xai_black_moneyness(
 	Function(XLL_DOUBLE, L"xll_black_moneyness", CATEGORY L".BLACK.MONEYNESS")
 	.Arguments({
@@ -70,6 +99,8 @@ double WINAPI xll_black_moneyness(double f, double s, double k, HANDLEX m)
 
 	return result;
 }
+
+// I feel like this is already TODO: implement OPTION.BLACK.PUT, I will try the CALL instead I guess
 
 AddIn xai_option_black_put(
 	Function(XLL_DOUBLE, L"xll_option_black_put", CATEGORY L".BLACK.PUT")
@@ -103,6 +134,101 @@ double WINAPI xll_option_black_put(double f, double s, double k, HANDLEX m)
 	return result;
 }
 
-// TODO: implement OPTION.BLACK.PUT
+// TODO: implement OPTION.BLACK.PUT (I will try CALL, as I see PUT is already up there)
 
-// TODO: implement OPTION.BSM.PUT
+AddIn xai_option_black_call(
+	Function(XLL_DOUBLE, L"xll_option_black_call", CATEGORY L".BLACK.CALL")
+	.Arguments({
+		Arg(XLL_DOUBLE, L"f", L"is the forward price."),
+		Arg(XLL_DOUBLE, L"s", L"is the volatility."),
+		Arg(XLL_DOUBLE, L"k", L"is the strike price."),
+		Arg(XLL_HANDLEX, L"m", L"is the handle to a model."),
+		})
+		.Category(CATEGORY)
+	.FunctionHelp(L"Return price of a European call option under the model.")
+);
+double WINAPI xll_option_black_call(double f, double s, double k, HANDLEX m)
+{
+#pragma XLLEXPORT
+	double result = NaN<double>;
+
+	try {
+		handle<model<>> m_(m);
+		ensure(m_);
+		result = black::call(f, s, k, *m_);
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+	catch (...) {
+		XLL_ERROR(__FUNCTION__ ": unknown exception");
+	}
+	return result;
+}
+
+// TODO: implement OPTION.BSM.PUT (also I will try with CALL after PUT)
+
+AddIn xai_option_bsm_put(
+	Function(XLL_DOUBLE, L"xll_option_bsm_put", CATEGORY L".BSM.PUT")
+	.Arguments({
+		Arg(XLL_DOUBLE, L"r", L"is the risk-free interest rate."),
+		Arg(XLL_DOUBLE, L"s0", L"is the spot price."),
+		Arg(XLL_DOUBLE, L"sigma", L"is the volatility."),
+		Arg(XLL_DOUBLE, L"k", L"is the strike price."),
+		Arg(XLL_DOUBLE, L"t", L"is the time to expiration."),
+		Arg(XLL_HANDLEX, L"m", L"is the handle to a model."),
+		})
+		.Category(CATEGORY)
+	.FunctionHelp(L"Return price of a European put option under the Black-Scholes-Merton model.")
+);
+double WINAPI xll_option_bsm_put(double r, double s0, double sigma, double k, double t, HANDLEX m)
+{
+#pragma XLLEXPORT
+	double result = NaN<double>;
+
+	try {
+		handle<model<>> m_(m);
+		ensure(m_);
+		result = black::bsm::put(r, s0, sigma, k, t, *m_);
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+	catch (...) {
+		XLL_ERROR(__FUNCTION__ ": unknown exception");
+	}
+	return result;
+}
+
+// BSM CALL here as well
+AddIn xai_option_bsm_call(
+	Function(XLL_DOUBLE, L"xll_option_bsm_call", CATEGORY L".BSM.CALL")
+	.Arguments({
+		Arg(XLL_DOUBLE, L"r", L"is the risk-free interest rate."),
+		Arg(XLL_DOUBLE, L"s0", L"is the spot price."),
+		Arg(XLL_DOUBLE, L"sigma", L"is the volatility."),
+		Arg(XLL_DOUBLE, L"k", L"is the strike price."),
+		Arg(XLL_DOUBLE, L"t", L"is the time to expiration."),
+		Arg(XLL_HANDLEX, L"m", L"is the handle to a model."),
+		})
+		.Category(CATEGORY)
+	.FunctionHelp(L"Return price of a European call option under the Black-Scholes-Merton model.")
+);
+double WINAPI xll_option_bsm_call(double r, double s0, double sigma, double k, double t, HANDLEX m)
+{
+#pragma XLLEXPORT
+	double result = NaN<double>;
+
+	try {
+		handle<model<>> m_(m);
+		ensure(m_);
+		result = black::bsm::call(r, s0, sigma, k, t, *m_);
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+	catch (...) {
+		XLL_ERROR(__FUNCTION__ ": unknown exception");
+	}
+	return result;
+}
