d6f4bedfd8c2375dd3b0b4d326a233056722844a
Kexin Tan <zoeytan1029@163.com>
2026-02-15 23:49:25 -0500

    hw3 finished

--- fms_option.h
+++ fms_option.h
@@ -67,7 +67,7 @@ namespace fms::option {
 		template<class F = double, class S = double, class K = double>
 		auto call(F f, S s, K k, const model<F, S>& m)
 		{
-			return 0; 
+			return put(f,s,k,m)+f-k; 
 		}
 
 		// In the Black-Scholes/Merton model
@@ -89,13 +89,29 @@ namespace fms::option {
 			inline auto moneyness(double r, double s0, double sigma, double k, double t,
 				const model<F,S>& m)
 			{
-				auto [f, s] = bsm_to_black(s0, r, sigma, t);
+				auto [f, s] = bsm_to_black(r, s0, sigma, t);
 				
 				return black::moneyness(f, s, k, m);
 			}
 
 			// TODO: implement bsm::put and bsm::call.
 			// Hint: use bsm_to_black to get f and s and then call black::put and black::call.
+			template<class F = double, class S = double>
+			inline auto put(double r, double s0, double sigma, double k, double t,
+				const model<F, S>& m)
+			{
+				auto [f, s] = bsm_to_black(r, s0, sigma, t);
+
+				return black::put(f,s,k,m);
+			}
+
+			template<class F = double, class S = double>
+			inline auto call(double r, double s0, double sigma, double k, double t,
+				const model<F, S>& m)
+			{
+				auto [f, s] = bsm_to_black(r, s0, sigma, t);
+				return black::call(f, s, k, m);
+			}
 		}
 	}
 
